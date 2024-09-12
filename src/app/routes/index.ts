ffd722f4ff9a9e427c50b71f29f5a0670de7381b
sabbir1054 <mdsabbir1054@gmail.com>
2024-09-12 10:14:55 +0600

    routes updated

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,18 +1,20 @@
 import express from 'express';
 import { CategoryRoutes } from '../modules/Category/category.route';
-import { CommissionRoutes } from '../modules/Commission/commission.route';
 import { ProductRoutes } from '../modules/Products/products.route';
-import { ReferredCodeRoutes } from '../modules/ReferredCode/referredCode.route';
-import { AdminRoutes } from '../modules/admin/admin.route';
 import { AuthRoutes } from '../modules/auth/auth.route';
 import { BusinessTypeRoutes } from '../modules/businessType/businessType.route';
 import { CartRoutes } from '../modules/cart/cart.routes';
 import { FeedbackRoutes } from '../modules/feedback/feedback.routes';
 import { OrderRoutes } from '../modules/order/order.route';
 import { OrganizationRoutes } from '../modules/organization/organization.route';
+import { PaymentOptionsRoutes } from '../modules/paymentOptions/paymentOptions.route';
 import { StaffRoutes } from '../modules/staff/staff.route';
 import { UsersRoutes } from '../modules/users/users.route';
 import { PointsValueRoutes } from '../modules/rewardPointsValue/pointsValue.route';
+import { AdminRoutes } from '../modules/admin/admin.route';
+import { CommissionRoutes } from '../modules/Commission/commission.route';
+import { RewardRoutes } from '../modules/reward/reward.route';
+import { ReferredCodeRoutes } from '../modules/ReferredCode/referredCode.route';
 import { ValidDaysRoutes } from '../modules/refeCodeValidDays/referCodeValidDays.route';
 
 const router = express.Router();
@@ -58,6 +60,34 @@ const moduleRoutes = [
   {
     path: '/staff',
     route: StaffRoutes,
+  },
+  {
+    path: '/my_admin',
+    route: AdminRoutes,
+  },
+  {
+    path: '/commission',
+    route: CommissionRoutes,
+  },
+  {
+    path: '/rewards',
+    route: RewardRoutes,
+  },
+  {
+    path: '/referredCode',
+    route: ReferredCodeRoutes,
+  },
+  {
+    path: '/points_value',
+    route: PointsValueRoutes,
+  },
+  {
+    path: '/validDays',
+    route: ValidDaysRoutes,
+  },
+  {
+    path: '/paymentOptions',
+    route: PaymentOptionsRoutes,
   },
 ];
 
