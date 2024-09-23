f47788ad978f7e270e7d2bd33a40ce4a17d03c25
sabbir1054 <mdsabbir1054@gmail.com>
2024-09-23 07:17:32 +0600

    order invoice route secure

--- src/app/modules/invoices/invoice.route.ts
+++ src/app/modules/invoices/invoice.route.ts
@@ -1,8 +1,23 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import { InvoiceController } from './invoice.controller';
 
 const router = express.Router();
 
-router.get('/genarate', InvoiceController.genaretInvoice);
+router.get(
+  '/genarate/:orderId',
+  auth(
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.DEALER,
+    ENUM_USER_ROLE.IMPORTER,
+    ENUM_USER_ROLE.MANUFACTURER,
+    ENUM_USER_ROLE.RESELLER,
+    ENUM_USER_ROLE.STAFF,
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.WHOLESALER,
+  ),
+  InvoiceController.genaretInvoice,
+);
 
 export const InvoceRoutes = router;
