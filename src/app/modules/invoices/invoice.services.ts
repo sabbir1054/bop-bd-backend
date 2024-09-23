311d1c935f4e51ab1a1518e9cd1a634111311f9c
sabbir1054 <mdsabbir1054@gmail.com>
2024-09-23 07:10:28 +0600

    order invoice done

--- src/app/modules/invoices/invoice.services.ts
+++ src/app/modules/invoices/invoice.services.ts
@@ -1,125 +1,60 @@
 import ejs from 'ejs';
 import { Request } from 'express';
 import fs from 'fs';
+import httpStatus from 'http-status';
 import path from 'path';
 import puppeteer from 'puppeteer';
+import ApiError from '../../../errors/ApiError';
+import prisma from '../../../shared/prisma';
 
 const generateInvoice = async (req: Request, res: any) => {
+  const { orderId } = req.params;
+  if (!orderId) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Order id not found');
+  }
+  const isExistOrder = await prisma.order.findUnique({
+    where: { id: orderId },
+    include: {
+      customer: true,
+      product_seller: true,
+      orderPaymentInfo: {
+        include: {
+          paymentSystemOptions: true,
+        },
+      },
+      orderItems: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+
+  if (!isExistOrder) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Order does not exist');
+  }
   const data = {
-    orderNumber: '0472',
-    orderDate: 'August 1, 2021',
+    orderNumber: isExistOrder.orderCode,
+    orderDate: isExistOrder.createdAt.toISOString().split('T')[0],
     companyLogoUrl:
       'https://bopbd.com.bd/_next/image?url=%2Fassets%2Fbopbdlogo.png&w=128&q=75',
     companyWebsite: 'bopbd.com.bd',
-    buyerLogoUrl:
-      'https://bopbd.com.bd/_next/image?url=%2Fassets%2Fbopbdlogo.png&w=128&q=75',
-    buyerName: 'Akij Group',
-    sellerLogoUrl:
-      'https://bopbd.com.bd/_next/image?url=%2Fassets%2Fbopbdlogo.png&w=128&q=75',
-    sellerName: 'Pran Ltd',
-    deliveryAddress:
-      'Khagan, Dhaka District, Dhaka Division, Bangladesh, Dhaka Division',
-    paymentMethod: 'BANK_TRANSACTION',
-    bankName: 'Bank Ashia',
-    accountNumber: '01268988798',
-    items: [
-      {
-        name: 'Pixel Design System',
-        description: 'Html components',
-        price: '128.00',
-        quantity: 1,
-        total: '128.00',
-      },
-      {
-        name: 'Volt Dashboard Template',
-        description: 'Tailwind template',
-        price: '69.00',
-        quantity: 1,
-        total: '69.00',
-      },
-      {
-        name: 'Volt Dashboard Template',
-        description: 'Tailwind template',
-        price: '69.00',
-        quantity: 1,
-        total: '69.00',
-      },
-      {
-        name: 'Volt Dashboard Template',
-        description: 'Tailwind template',
-        price: '69.00',
-        quantity: 1,
-        total: '69.00',
-      },
-      {
-        name: 'Volt Dashboard Template',
-        description: 'Tailwind template',
-        price: '69.00',
-        quantity: 1,
-        total: '69.00',
-      },
-      {
-        name: 'Volt Dashboard Template',
-        description: 'Tailwind template',
-        price: '69.00',
-        quantity: 1,
-        total: '69.00',
-      },
-      {
-        name: 'Volt Dashboard Template',
-        description: 'Tailwind template',
-        price: '69.00',
-        quantity: 1,
-        total: '69.00',
-      },
-      {
-        name: 'Volt Dashboard Template',
-        description: 'Tailwind template',
-        price: '69.00',
-        quantity: 1,
-        total: '69.00',
-      },
-      {
-        name: 'Volt Dashboard Template',
-        description: 'Tailwind template',
-        price: '69.00',
-        quantity: 1,
-        total: '69.00',
-      },
-      {
-        name: 'Volt Dashboard Template',
-        description: 'Tailwind template',
-        price: '69.00',
-        quantity: 1,
-        total: '69.00',
-      },
-      {
-        name: 'Volt Dashboard Template',
-        description: 'Tailwind template',
-        price: '69.00',
-        quantity: 1,
-        total: '69.00',
-      },
-      {
-        name: 'Volt Dashboard Template',
-        description: 'Tailwind template',
-        price: '69.00',
-        quantity: 1,
-        total: '69.00',
-      },
-      {
-        name: 'Volt Dashboard Template',
-        description: 'Tailwind template',
-        price: '69.00',
-        quantity: 1,
-        total: '69.00',
-      },
-    ],
-    subtotal: '415.00',
-    deliveryFee: '50',
-    total: '465.00',
-    supportEmail: 'support@gmail.com',
-    supportPhone: '012478963254',
+    buyerLogoUrl: isExistOrder.customer.photo,
+    buyerName: isExistOrder.customer.name,
+    sellerLogoUrl: isExistOrder.product_seller.photo,
+    sellerName: isExistOrder.product_seller.name,
+    deliveryAddress: isExistOrder.shipping_address,
+    paymentMethod:
+      isExistOrder.orderPaymentInfo?.paymentSystemOptions.paymentCategory,
+    bankName: isExistOrder.orderPaymentInfo?.paymentSystemOptions.methodName,
+    accountNumber:
+      isExistOrder.orderPaymentInfo?.paymentSystemOptions.accountNumber,
+    items: isExistOrder.orderItems,
+    subtotal: isExistOrder.total,
+    deliveryFee: isExistOrder.deliveryCharge,
+    total: isExistOrder.totalWithDeliveryCharge,
+    supportEmail: 'support@bopbd.com.bd',
+    supportPhone: '+8801969669908',
   };
 
   try {
