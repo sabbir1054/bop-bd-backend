567ef65e1c4bfbb7ec30829bcb99b64a93d79f55
sabbir1054 <mdsabbir1054@gmail.com>
2024-10-31 12:27:03 +0600

    sms balance update

--- src/helpers/smsBalanceCheck.ts
+++ src/helpers/smsBalanceCheck.ts
@@ -1,8 +1,18 @@
 import axios from 'axios';
+import httpStatus from 'http-status';
 import config from '../config';
+import ApiError from '../errors/ApiError';
 
 export const chackSmsBalance = async () => {
   const balanceCheckLink = config.mobileOTP.smsBalance;
-  const response = await axios.get(`${balanceCheckLink}`);
-  return response;
+  try {
+    if (balanceCheckLink) {
+      const response = await axios.get(balanceCheckLink);
+
+      // Return only the data part of the response
+      return response.data;
+    }
+  } catch (error) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Error checking SMS balance:');
+  }
 };
